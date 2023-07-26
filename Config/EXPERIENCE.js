ad981d2cd5acf19ac17752221c1a120c3c92bf03
Gradixnt.pxrk <prem.premsri@gmail.com>
2023-07-26 20:35:09 +0700

    edit .

---
+++ Config/EXPERIENCE.js
@@ -0,0 +1,20 @@
+const EXPERIENCE = [
+
+    {
+        Topic_EN: "VIP MENU",
+        Topic_TH: "ระบบวีไอพี",
+        IMAGE: "https://cdn.discordapp.com/attachments/1063743776536928379/1133751642651627652/image.png",
+        GITHUB: "https://github.com/Pxrkmylove",
+        YOUTUBE: "https://youtu.be/VRvA0XsubtI",
+        FILE: "HASHTAG_VIP"
+    },
+    {
+        Topic_EN: "ESPORT ZONE",
+        Topic_TH: "โซนเเข่ง",
+        IMAGE: "https://cdn.discordapp.com/attachments/1064902896241619056/1120278727000084480/image.png",
+        GITHUB: "https://github.com/Pxrkmylove",
+        YOUTUBE: "https://youtu.be/ncC885lrVf4",
+        FILE: "HASHTAG_ESPORTZONE+"
+    },
+
+]
--- Source/app.js
+++ Source/app.js
@@ -31,4 +31,37 @@ $(document).ready(function() {
         // เปิดโปรแกรมอีเมลหรือ Mailto URL โดยใช้ window.open()
         window.open('mailto:prem.premsri@gmail.com');
     });
+
+    EXPERIENCE.forEach((value, index) => {
+
+
+        $('.experience-page').append(`
+            <div class="box-show-experience">
+                <span id="topic">
+                    <h2 style="color: antiquewhite;">${value.Topic_EN}</h2><p>(${value.Topic_TH})</p>
+                </span>
+                <div class="box-link">
+                    <a href="${value.IMAGE}" target="_blank">
+                        <p><i class="fa-regular fa-image"></i> Image</p>
+                    </a>
+                </div>
+                <div class="box-link">
+                    <a href="${value.GITHUB}" target="_blank">
+                        <p><i class="fa-brands fa-github" style="color: #000000;"></i> Github</p>
+                    </a>
+                </div>
+                <div class="box-link">
+                    <a href="${value.YOUTUBE}" target="_blank">
+                        <p><i class="fa-brands fa-youtube" style="color: #ff0000;"></i> Youtube</p>
+                    </a>
+                </div>
+                <div class="box-link">
+                    <a href="Source/files/${value.FILE}.rar" download target="_blank">
+                        <p><i class="fa-duotone fa-file" style="--fa-primary-color: #52cbff; --fa-primary-opacity: 0.4; --fa-secondary-color: #43a6d0; --fa-secondary-opacity: 1;"></i> File</p>
+                    </a>
+                </div>
+            </div>
+        `);
+
+    });
 });
