0e31071bfadad962694bd48722a6060f8216f0dc
Gradixnt.pxrk <prem.premsri@gmail.com>
2023-07-26 21:20:31 +0700

    edit .

--- Source/app.js
+++ Source/app.js
@@ -79,3 +79,50 @@ experiences.forEach((value) => {
 
 const experiencePage = document.querySelector('.experience-page');
 experiencePage.innerHTML = htmlContent;
+
+let toggle = "sun";
+
+function darkmode() {
+    const btnDarkLight = document.querySelector(".btn-dark-light");
+    const iconDark = `<p id="icon-dark"><i class="fa-regular fa-${toggle === "sun" ? "moon" : "sun"}" style="color: ${toggle === "sun" ? "#ffd60a" : "red"};"></i></p>`;
+
+    if (toggle === "sun") {
+        btnDarkLight.innerHTML = "";
+        btnDarkLight.style.backgroundColor = "rgb(50, 50, 50)";
+        btnDarkLight.innerHTML = iconDark;
+
+        document.body.style.color = "black";
+        document.querySelectorAll(".category, .performance").forEach((el) => {
+            el.style.backgroundColor = "rgba(232, 232, 232, 0.981)";
+        });
+
+        document.querySelectorAll(".box-show-experience").forEach((el) => {
+            el.style.backgroundColor = "rgb(156, 155, 155)";
+        });
+
+        document.querySelectorAll(".box-link").forEach((el) => {
+            el.style.backgroundColor = "rgba(118, 118, 118, 0.781)";
+        });
+
+        toggle = "night";
+    } else if (toggle === "night") {
+        btnDarkLight.style.backgroundColor = "rgb(217, 217, 217)";
+        btnDarkLight.innerHTML = "";
+        btnDarkLight.innerHTML = iconDark;
+
+        document.body.style.color = "whitesmoke";
+        document.querySelectorAll(".category, .performance").forEach((el) => {
+            el.style.backgroundColor = "rgba(122, 122, 122, 0.981)";
+        });
+
+        document.querySelectorAll(".box-show-experience").forEach((el) => {
+            el.style.backgroundColor = "rgb(95, 95, 95)";
+        });
+
+        document.querySelectorAll(".box-link").forEach((el) => {
+            el.style.backgroundColor = "rgba(55, 54, 54, 0.781)";
+        });
+
+        toggle = "sun";
+    }
+}
